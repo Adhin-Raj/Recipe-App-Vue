4f8331eff7d62defc8d0e21ab3cee2e18ee2a16b
Adhin Raj M <adhin.raj@vonnue.com>
2025-11-11 17:57:15 +0530

    feat: added header component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
-<script setup></script>
+<script setup>
+import Header from './components/atoms/Header/Header.vue';
+
+
+</script>
 
 <template>
-  
+   <Header />
 </template>
 
 <style scoped></style>
